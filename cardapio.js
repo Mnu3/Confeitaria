55a0c6dac410ab403ab084249810c534b422df48
Manuela Aguiar Gil <manuelaaguiargil06@gmail.com>
2025-11-26 20:15:05 -0300

    criando item com js

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,29 @@
-alert('oi fui carregado')
+function criarItemCardapio(titulo, descricao, foto){
+    const divItemCardipio = document.createElement('div')
+    divItemCardipio.className = 'item-cardapio'
+
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
+
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = descricao
+    pDescricao.className = 'descricao'
+
+    const img = document.createElement('img')
+    img.src = foto
+    img.className = 'img-item'
+
+    const divC = document.getElementById('cardapio')
+
+    divItemCardipio.appendChild(h3Titulo)
+    divItemCardipio.appendChild(pDescricao)
+    divItemCardipio.appendChild(img)
+
+    divC.appendChild(divItemCardipio)
+}
+criarItemCardapio(
+    'Bolo de Chocolate',
+    'Um clássico irresistível com camadas de chocolate',
+    'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg'
+
+)
